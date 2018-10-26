4b8f7f677718df49355182b67d8a2d30617aa72f
caoson2798 <ygiangson@gmail.com>
2018-10-26 21:08:56 +0100

    tao mới sửa

--- SOURCE/app/src/main/java/com/example/daiphongpc/androidgopro/MainActivity.java
+++ SOURCE/app/src/main/java/com/example/daiphongpc/androidgopro/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Day là dồngadas
+        //dsadadsda
     }
 }
